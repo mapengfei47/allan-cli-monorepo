f373cca2961a173aa7460ab23acd80f3582e4bfc
allanma <allanma@xiaoe-tech.com>
2024-11-06 22:34:01 +0800

    feat: 增加cli 和 create

---
+++ packages/create/src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: mapengfei
+ * @Date: 2024-11-06 22:25:40
+ * @LastEditors: mapengfei
+ * @LastEditTime: 2024-11-06 22:25:42
+ */
+async function create(){
+  console.log('create 命令执行中...')
+}
+
+export default create
